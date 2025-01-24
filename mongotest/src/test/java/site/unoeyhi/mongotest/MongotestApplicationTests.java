ba3a88c2eb67497e0c6d59ec0807c3ba45a17e29
ihyeonu2312 <dlgusdn2312@gmail.com>
2025-01-24 16:50:47 +0900

    몽고db

---
+++ mongotest/src/test/java/site/unoeyhi/mongotest/MongotestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.unoeyhi.mongotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
